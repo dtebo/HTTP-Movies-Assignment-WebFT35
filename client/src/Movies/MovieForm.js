cd66851d8561f07a42c21de5c84d9a2e532f1560
Darren Tebo <darrentebo83@outlook.com>
2020-10-14 16:05:27 -0400

    added form styling

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import './Movies.css';
 
 const initialValues = {
     title: '',
     director: '',
-    metascore: 0,
+    metascore: '0',
     stars: [],
     error: ''
 };
 
 const MovieForm = props => {
     const [formValues, setFormValues] = useState(initialValues);
+    const params = useParams();
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/api/movies/${params.id}`)
+            .then(res => {
+                console.log('MovieForm: DT: useEffect: ', res);
+            })
+            .catch(err => console.error('MovieForm: DT: useEffect: Error: ', err));
+    }, []);
 
     const handleChanges = e => {
 
@@ -21,7 +35,39 @@ const MovieForm = props => {
 
     return (
         <>
-            <h1>MovieForm here...</h1>
+            <form 
+                className='movie-form'
+                onSubmit={handleSubmit}
+            >
+                <section className='form-group'>
+                    <label htmlForm='title'>Title</label>
+                    <label htmlFor='director'>Director</label>
+                    <label htmlFor='metascore'>MetaScore</label>
+                </section>
+                <section className='form-group'>
+                    <input
+                        type='text'
+                        id='title'
+                        name='title'
+                        value={formValues.title}
+                        onChange={handleChanges}
+                    />
+                    <input
+                        type='text'
+                        id='director'
+                        name='director'
+                        value={formValues.director}
+                        onChange={handleChanges}
+                    />
+                    <input
+                        type='text'
+                        id='metascore'
+                        name='metascore'
+                        value={formValues.metascore}
+                        onChange={handleChanges}
+                    />
+                </section>
+            </form>
         </>
     );
 };
